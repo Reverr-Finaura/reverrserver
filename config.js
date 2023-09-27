47e12cec228e3a505c6d07c3957be674a7d49a61
Maurice522 <mauricerana@gmail.com>
2023-09-27 22:11:31 +0530

    added checker

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore,  FieldValue ,Timestamp} = require('firebase-admin/firestore');
-const serviceAccount = require('./reverr-25fb3-firebase-adminsdk-g8tph-a9d7f58699.json');
+// const serviceAccount = require('./reverr-25fb3-firebase-adminsdk-g8tph-a9d7f58699.json');
+const serviceAccount = require('./dsquare-242c3-firebase-adminsdk-dgcnn-eba5c18dc3.json');
 const { getStorage } = require('firebase-admin/storage');
 
 const app = initializeApp({
